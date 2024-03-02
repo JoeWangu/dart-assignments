7aa06232616b0dbb6784ae13e5eec1d5404ad0a8
Joel-009 <79811019+Joel-009@users.noreply.github>
2024-03-02 14:47:30 +0300

    assignment wk3

--- plp-wk3-assignment.dart
+++ plp-wk3-assignment.dart
@@ -1,14 +1,25 @@
 import 'dart:io';
 
 void main() {
-  print('Enter a number: ');
-  int num = int.parse(stdin.readLineSync()!);
+  // Get user input
+  print("Enter a number: ");
+  String? input = stdin.readLineSync();
 
-  if (num > 10) {
-    print('Your number is greater than 10');
-  } else if (num < 10) {
-    print('Your number is less than 10');
+  // Convert input to number (handle potential errors)
+  double? number;
+  try {
+    number = double.parse(input!);
+  } catch (e) {
+    print("Invalid input: Please enter a valid number.");
+    return;
+  }
+
+  // Check and print message based on the number
+  if (number > 10) {
+    print("Your number is greater than 10.");
+  } else if (number < 10) {
+    print("Your number is less than 10.");
   } else {
-    print('Your number is equal to 10');
+    print("Your number is equal to 10.");
   }
 }
